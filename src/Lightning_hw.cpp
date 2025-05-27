92ecc9040804e1abb43fc0d0aa8052224744f91e
rpbgit <n612dw@gmail.com>
2025-05-26 22:50:50 -0500

    v4.32

--- src/Lightning_hw.cpp
+++ src/Lightning_hw.cpp
@@ -80,11 +80,12 @@ try to remember to bump this each time a functional mod is done
                             floating point distance to storm. 
 21-May-2025 w9zv    v4.31   added tracking of max rate of events in station_management. refactor station_management: implement circular buffers for event 
                             timestamps and improve rate calculations
+26-May-2025 w9zv    v4.32   changed setup2() to ensure factory defaults for all config registers for AS3935 device are set.
 */
 
 // define the version of the code which is displayed on TFT/Serial/and web page. This is the version of the code, not the hardware.
 // pse update this whenver a new version of the code is released.
-constexpr const char* CODE_VERSION_STR = "v4.31";  // a string for the application version number
+constexpr const char* CODE_VERSION_STR = "v4.32";  // a string for the application version number
 
 // a widget to stop/hold further execution of the code until we get sent something from the host
 // it will also print out the line of source code it is being executed in.
